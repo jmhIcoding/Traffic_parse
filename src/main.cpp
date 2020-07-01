4e4b7f951624ba5c20466299b08519cdb17c909b
dkT430 <jiangminghao@iie.ac.cn>
2020-07-01 22:25:59 +0800

    update

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,7 @@ void print_payload(unsigned char *data, int len)
 		}
 	}
 }
-int clean_pcap(char *pcapname, char * filter = "host 47.100.21.91 and(tcp or udp)",char redirect=1)
+int clean_pcap(char *pcapname, char * filter = "host 47.100.21.91 and(tcp or udp)",char redirect=0)
 {
 	pcap_gather gather = pcap_gather(pcapname);
 	if (gather.pcapt == NULL)
@@ -40,7 +40,7 @@ int clean_pcap(char *pcapname, char * filter = "host 47.100.21.91 and(tcp or udp
 		gather.get_next_packet(&packet);
 		if (!redirect)
 		{
-			printf("id:%d\t", packetno);
+			;// printf("\nid:%d\t", packetno);
 		}
 
 		//if (packetno == 1203)
@@ -131,6 +131,108 @@ int clean_pcap(char *pcapname, char * filter = "host 47.100.21.91 and(tcp or udp
 				}
 				
 			}
+			else
+				//说明是NFLOG的链路层,因为限定网络层必须为IP协议。
+			{	
+				if (packet.data[0] != 0x02)
+				{
+					break;
+				}
+				int offsize = 0;
+				for (offsize = 4; offsize < packet.len;)
+				{
+					int length = packet.data[offsize] + packet.data[offsize + 1]*256 ;
+					int type = packet.data[offsize + 2]*256 + packet.data[offsize + 3] ;
+					//printf("len=%d,type=%X\n", length, type);
+					if (type == 0x0900)
+					{
+						offsize += 4;//最后跳过
+						break;
+					}
+					if (length % 4 == 0)
+					{
+						offsize += length;
+					}
+					else
+					{
+						offsize += (length + 4) / 4 * 4;
+					}
+
+
+				}
+				
+				ip_header ip = ip_parser(packet.data + offsize);//parse ip header
+				
+				if (ip.proto == 0x11)
+					//udp
+				{
+					udp_header udp = udp_parser(packet.data + offsize+4 * (ip.ver_ihl & 0xF));//parse udp header
+					if (udp.len - 8)
+					{
+						printf("%d.%d,", packet.timestamp, packet.usec);
+						DbgPrint(ip_info, &ip);
+						DbgPrint(udp_info, &udp);
+						printf("%d,", udp.len - 8);
+						//print_payload(packet.data + sizeof(ethII_header)+4 * (ip.ver_ihl & 0xF) + 8, udp.len - 8);
+						printf("\n");
+					}
+				}
+				else if (ip.proto == 0x06)
+					//tcp
+				{
+					tcp_header tcp = tcp_parser(packet.data + offsize +4 * (ip.ver_ihl & 0xF));//parse tcp header
+					int len = ip.tlen - 4 * (ip.ver_ihl & 0xF) - 4 * ((tcp.tcpHeader_reserve & 0xF0) >> 4);
+					if (len>0 && len <= 1460)
+					{
+						printf("%d.%d,", packet.timestamp, packet.usec);
+						DbgPrint(ip_info, &ip);
+						DbgPrint(tcp_info, &tcp);
+						printf("%d,", len);
+						printf("\n");
+					}
+					else if (len > 1460)
+					{
+						int i;
+						for (i = 0; i < (len / 1460); i++)
+						{
+							printf("%d.%d,", packet.timestamp, packet.usec);
+							DbgPrint(ip_info, &ip);
+							DbgPrint(tcp_info, &tcp);
+							printf("%d,", 1460);
+							printf("\n");
+						}
+						if (i * 1460 < len)
+						{
+							printf("%d.%d,", packet.timestamp, packet.usec);
+							DbgPrint(ip_info, &ip);
+							DbgPrint(tcp_info, &tcp);
+							printf("%d,", len - i * 1460);
+							printf("\n");
+						}
+					}
+					else if (len<0)
+						//need to frack.
+					{
+						int i;
+						for (i = 0; i < (packet.len / 1460); i++)
+						{
+							printf("%d.%d,", packet.timestamp, packet.usec);
+							DbgPrint(ip_info, &ip);
+							DbgPrint(tcp_info, &tcp);
+							printf("%d,", 1460);
+							printf("\n");
+						}
+						if (i * 1460 < packet.len)
+						{
+							printf("%d.%d,", packet.timestamp, packet.usec);
+							DbgPrint(ip_info, &ip);
+							DbgPrint(tcp_info, &tcp);
+							printf("%d,", packet.len - i * 1460);
+							printf("\n");
+						}
+					}
+				}
+			}
 			packetno++;
 		}
 		else
@@ -155,26 +257,19 @@ int main(int argc,char *argv[])
 	//printf("Time Use:%d\n", end - start);
 	//return 0;
 
-	if (argc != 3)
+	if (argc != 2)
 	{
-		printf("[usage]:vsrcs.exe srcpcap_dir filter\n");
+		printf("[usage]:vsrcs.exe pcap_name \n");
 		exit(-1);
 	}
-	char PCAPDIR_[230] = { 0 };
-	sprintf(PCAPDIR_, "%s\\*", argv[1]);
-	vector<string> files = get_files_from_dir(PCAPDIR_,".pcap");
-	PCAPDIR_[strlen(PCAPDIR_) - 1] = 0;
-	for (int i = 0; i < files.size(); i++)
+	
+	char * pcapname = argv[1];
+	//char * pcapname = "E:\\graph_neural_network_over_smartphone_application\\Traffic_graph_generator\\tmp.pcap";
+	if (clean_pcap(pcapname,NULL,0) == -1)
 	{
-		char pcapname[256] = { 0 };
-		freopen("CON", "w", stdout);
-		sprintf(pcapname, "%s%s", PCAPDIR_, files[i].c_str());
-		printf("(%0.3f/100)\t%s\n",i*100.0/files.size(), pcapname);
-		if (clean_pcap(pcapname, argv[2], 1) == -1)
-		{
-			printf("Error!!!!%s\n", files[i].c_str());
-		}
+			printf("Error!!!!%s\n", pcapname);
 	}
+
 	//system("pause");
 	return 0;
 }
